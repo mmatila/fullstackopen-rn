542167e0696ec8c1eb12e0a4a8a220d1ef9ad43c
Manu Matila <manu.matila1997@gmail.com>
2022-01-26 13:26:55 +0200

    10.10 done

--- src/theme.js
+++ src/theme.js
@@ -1,3 +1,5 @@
+import { Platform } from "react-native";
+
 export const theme = {
   colors: {
     primary: '#0366d6',
@@ -15,9 +17,11 @@ export const theme = {
     subheading: 16,
     paragraph: 12,
   },
-  fonts: {
-    main: 'System',
-  },
+  fonts: Platform.select({
+    ios: 'Arial',
+    android: 'Roboto',
+    default: 'System'
+  }),
   fontWeights: {
     normal: '400',
     bold: '700',
